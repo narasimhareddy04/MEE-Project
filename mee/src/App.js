5e53af24ed2d77e777aa3e6471c720f8c3364bea
Mohana Narasimha Reddy Attunuru <mattunur@asu.edu>
2023-04-19 14:53:37 -0700

    Adding Tailwind

--- mee/src/App.js
+++ mee/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main class="flex justify-center gap-4 flex-col min-h-screen">
+      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
+      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
+      <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
+    </main>
   );
 }
 
